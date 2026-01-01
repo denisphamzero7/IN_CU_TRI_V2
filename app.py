b7fa65e7e8833108fa0f0e41400564394b2d11d7
denisphamzero7 <pnhau2001@gmail.com>
2026-01-01 13:47:45 +0700

    ttkbootrap app

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 # app.py
 import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from config.settings import DEFAULT_THEME
 from views.main_view import MainView
 from controllers.router import AppRouter
 
-class App(tk.Tk):
+class App(ttk.Window):
     def __init__(self):
-        super().__init__()
+        super().__init__(themename=DEFAULT_THEME)
         self.title("HỆ THỐNG IN THẺ CỬ TRI ")
         self.geometry("1600x900")
         
@@ -14,7 +17,9 @@ class App(tk.Tk):
         
         # 2. Khởi tạo View (Giao diện), truyền Router vào
         self.main_view = MainView(self, self.router)
-        
+        # 3. QUAN TRỌNG: Phải đặt View lên cửa sổ
+        # Nếu thiếu dòng này, giao diện sẽ trống trơn
+        self.main_view.pack(fill=BOTH, expand=YES)
         # 3. Kết nối ngược View vào Router
         self.router.set_view(self.main_view)
 
